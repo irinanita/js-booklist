d1f356a717dedfc73662be1e28ad93322f1e77f0
irina <irina@pond5.com>
2021-05-25 21:29:03 +0100

    Get values from the form.Instantiate book object

---
+++ app.js
@@ -0,0 +1,23 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+// Event listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+    // Instantiating a book object
+    const book = new Book(title,author,isbn);
+
+  console.log(book);
+  e.preventDefault();
+});
