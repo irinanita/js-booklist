1140cc897fc8a0bb73cb3937a1c84db005bf6b39
irina <irina@pond5.com>
2021-05-25 22:02:32 +0100

    add book to list functionality

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ function Book(title, author, isbn) {
 // UI constructor
 function UI() {}
 
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  // Create tr element
+  const row = document.createElement("tr");
+  // Insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>`;
+  list.appendChild(row);
+};
+
+UI.prototype.clearFields = function (){
+  document.getElementById("title").value="";
+  document.getElementById("author").value="";
+  document.getElementById("isbn").value="";
+}
+
+// Instantiate UI
+const ui = new UI();
+
 // Event listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
@@ -15,9 +37,14 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     author = document.getElementById("author").value,
     isbn = document.getElementById("isbn").value;
 
-    // Instantiating a book object
-    const book = new Book(title,author,isbn);
+  // Instantiating a book object
+  const book = new Book(title, author, isbn);
 
-  console.log(book);
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear input fields
+  ui.clearFields();
+
   e.preventDefault();
 });
